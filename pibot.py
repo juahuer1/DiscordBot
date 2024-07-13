55ebea063e8f0b061164532648b52916d227c1eb
juahuer1 <juahuer1@teleco.upv.es>
2024-07-13 08:43:27 +0200

    Replicamos todos los comandos slash

--- pibot.py
+++ pibot.py
@@ -203,27 +203,107 @@ async def clearaudio(ctx):
     voice_client.stop()
     
 # Define un comando de barra (slash command)
+@bot.tree.command(name="add", description="Suma dos numeros (Ex: /add 5 11)")
+async def add(interaction: discord.Interaction, left: int, right: int):
+    result = left + right
+    await interaction.response.send_message(str(result))
 
-# Define un comando de barra (slash command)
-@bot.tree.command(name="hello", description="Di hola")
-async def hello(interaction: discord.Interaction):
-    await interaction.response.send_message(f'Hola {interaction.user.mention}!')
-
-# Define otro comando de barra (slash command)
-@bot.tree.command(name="goodbye", description="Di adi")
-async def goodbye(interaction: discord.Interaction):
-    await interaction.response.send_message(f's {interaction.user.mention}!')
-
-# Define un tercer comando de barra (slash command)
-@bot.tree.command(name="ping", description="Muestra el ping del bot")
-async def ping(interaction: discord.Interaction):
-    latency = bot.latency
-    await interaction.response.send_message(f'Pong! Latencia: {latency * 1000:.2f}ms')
     
-# Define un tercer comando de barra (slash command)
-@bot.tree.command(name="peroseras", description="Muestra el ping del bot")
-async def peroseras(interaction: discord.Interaction):
-    latency = bot.latency
-    await interaction.response.send_message(f'Pong! Latencia: {latency * 1000:.2f}ms')
+@bot.tree.command(name="roll", description="Tira N dados de N caras en este orden NdN (dados, caras) (Ex: /roll 3d6)")
+async def roll(interaction: discord.Interaction, dice: str):
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await interaction.response.send_message('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await interaction.response.send_message(result)
+
+
+@bot.tree.command(name="choose",description='Elige al azar entre varias opciones (Ex: /choose Pizza Pasta Hamburguesa)')
+async def choose(interaction: discord.Interaction, choices: str):
+    chosen = random.choice(choices.split())
+    await ctx.send(chosen)
+
+@bot.tree.command(name="repeat", description="Repite lo mismo muchas veces (Ex: /repeat 5 Feo)")
+async def repeat(interaction: discord.Interaction, times: int, content: str = 'repeating...'):
+    for i in range(times):
+        await interaction.response.send_message(content)
+        await interaction.followup.send(content)
+
+@bot.tree.command(name="joined", description="Fecha de inclusion de un miembro (Ex: /joined juanmingla)")
+async def joined(interaction: discord.Interaction, member: discord.Member):
+    await interaction.response.send_message(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+
+@bot.tree.command(name='bot', description="Mola mi bot u que? (Ex: /bot)")
+async def _bot(interaction: discord.Interaction):
+    await interaction.response.send_message('Es la peripocha.')
+
+
+@bot.tree.command(name="join", description="Agrega el bot al chat (Ex: /join)")
+async def join(interaction: discord.Interaction):
+    if interaction.author.voice:
+        channel = interaction.author.voice.channel
+        await channel.connect()
+        await interaction.response.send_message("Bot spawneando pa molestar a AiramariA")
+    else:
+        await interaction.response.send_message("Bot no se puede unir, metete en un canal de audio!")
+
+
+@bot.tree.command(name="leave", description="Elimina el bot del chat (Ex: /leave)")
+async def leave(interaction: discord.Interaction):
+    if interaction.guild.voice_client:
+        await interaction.guild.voice_client.disconnect()
+        await interaction.response.send_message("El putisimo Bot se pira")
+    else:
+        await interaction.response.send_message("Bot no esta en el canal")
+
+
+@bot.tree.command(name='audios', description="Reproduce audios en el canal en uso (Ex: /audios)")
+async def audios(interaction: discord.Interaction):
+    voice_client = interaction.guild.voice_client
+    if not voice_client:
+        await join(interaction)  # Llama a la funcion join si no esta conectado
+        voice_client = interaction.guild.voice_client  # Actualiza el cliente de voz
+    
+    audio_player = AudioPlayer(voice_client) 
+    system_functions = SystemFunctions()  
+    view = AudioView(audio_player, system_functions)  
+    await interaction.response.send_message("Elige una opcion del menu:", view=view)
+
+
+@bot.tree.command(name='clearaudio', description="Interrumpimos audio en reproduccion (Ex: /clearaudio)")
+async def clearaudio(interaction: discord.Interaction):
+    voice_client = interaction.guild.voice_client
+    if voice_client:
+        voice_client.stop()
+        await interaction.response.send_message("Audio interrumpido")
+    else:
+        await interaction.response.send_message("No hay audio reproduciendose")
+
+
+# # Define un comando de barra (slash command)
+# @bot.tree.command(name="hello", description="Di hola")
+# async def hello(interaction: discord.Interaction):
+    # await interaction.response.send_message(f'Hola {interaction.user.mention}!')
+
+# # Define otro comando de barra (slash command)
+# @bot.tree.command(name="goodbye", description="Di adi")
+# async def goodbye(interaction: discord.Interaction):
+    # await interaction.response.send_message(f's {interaction.user.mention}!')
+
+# # Define un tercer comando de barra (slash command)
+# @bot.tree.command(name="ping", description="Muestra el ping del bot")
+# async def ping(interaction: discord.Interaction):
+    # latency = bot.latency
+    # await interaction.response.send_message(f'Pong! Latencia: {latency * 1000:.2f}ms')
+    
+# # Define un tercer comando de barra (slash command)
+# @bot.tree.command(name="peroseras", description="Muestra el ping del bot")
+# async def peroseras(interaction: discord.Interaction):
+    # latency = bot.latency
+    # await interaction.response.send_message(f'Pong! Latencia: {latency * 1000:.2f}ms')
 
 bot.run(bottoken)
