231338afff574a3f170f3091ed19d334c2cf28b1
P-Sergio <serpamvi@teleco.upv.es>
2024-08-19 19:46:37 +0200

    Añadimos frases de Moe para el panel de audios

--- src/thematic.py
+++ src/thematic.py
@@ -18,8 +18,12 @@ class InitEnv():
                          "silent": False,
                          "audio_panel_title": "Bar de Moe, Moe al habla",
                          "audio_panel_description": [
-                            "*Señor Reves? de nombre Stal*, \n Un momento, A VEEER STAL REVES, alguno de ustedes Stal Reves?",
-                            "*¿Está Topocho? De nombre Donpi* \n Deja que pregunte. Donpi Topocho, ¿ES QUE NADIE AQUÍ ES UN DONPI TOPOCHO?"
+                            "*¿Señor Reves? de nombre Stal*, \n Un momento, A VEEER STAL REVES, alguno de ustedes Stal Reves?",
+                            "*¿Está Topocho? De nombre Donpi* \n Deja que pregunte. Donpi Topocho, ¿ES QUE NADIE AQUÍ ES UN DONPI TOPOCHO?",
+                            "*¿Está el señor Riau? De nombre Smith* \n Un momento, voy a ver. ¿Hay aquí algún Smith Riau? ¿NO ME OÍS? ¿¡ALGUNO SOIS SMITH RIAU!?",
+                            "*¿Está Empel? De nombre Otas* \n Voy a ver. ¡QUE SE PONGA AL TELÉFONO EMPEL OTAS!",
+                            "*Pregunto por el señor Ollas, de nombre Philip* \n Sí, un minuto, voy a ver. PHILIP OLLAS, PHILIP OLLAS. Venga, chicos, ¿no hay ningún Philip Ollas por aquí?",
+                            "*Quisiera hablar con la señora Chondo. ¿De nombre? Stoika \n Un segundo. STOIKA CHONDO, STOIKA CHONDO. Vamos, señora, creo que esto va por usted ¡STOIKA CHONDO!"
                          ],
                          "audio_panel_image_path": "./Imagenes/Simpsons/moe_al_habla.jpg",
                          "audio_panel_image_name": "moe_al_habla.jpg",
